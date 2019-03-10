6d4c0aab584e73dd2e60eb1276a07089b72c7663
greyCimath <grey.qisen@gmail.com>
2019-03-11 00:01:10 +0800

    20190311-0000

--- lintcode_993.cpp
+++ lintcode_993.cpp
@@ -4,24 +4,42 @@
 
 using namespace std;
 
+template<typename T>
+ostream& operator<<(ostream& os,const vector<T>& v){
+    for(int i = 0;i < v.size();i++){
+        os << v[i] << " ";
+    }
+    return os;
+}
+
 int arrayPairSum(vector<int> &nums){
     int pos = 0;    int ans = 0;
     sort(nums.begin(),nums.end());
-    while(pos < nums.size()){
-        ans += (nums[pos] ? nums[pos + 1] : nums[pos] <= nums[pos + 1]);
-        cout << ans << endl;
-        pos += 2;
+    for(pos = 0;pos < nums.size();pos += 2){
+        ans += nums[pos];
     }
+
     return ans;
 }
+
+/*
+int readInt(){
+    int x = 0;
+    cin >> x;
+    return x;
+}
+*/
 
 int main(){
     vector<int> nums;   int n = 0;  int tmp = 0;
     cin >> n;
-    while(--n){
+    n *= 2;
+    while(n--){
         cin >> tmp;
         nums.push_back(tmp);
     }
+    //cout << nums;
+
     tmp = arrayPairSum(nums);
     cout << tmp << endl;
 
